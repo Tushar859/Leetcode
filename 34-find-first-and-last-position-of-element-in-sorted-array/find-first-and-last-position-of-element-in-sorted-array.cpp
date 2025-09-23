4153a41d30f0ba27b744422c5eb734b12709b3fd
Tushar859 <mishrat859@gmail.com>
2025-09-23 20:58:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.76%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        vector<int> ans;
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]==target){
-                ans.push_back(i);
-                
+    int findFirst(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1, first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                first = mid;
+                right = mid - 1; // keep looking left
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
-            
+        }
+        return first;
+    }
 
+    int findLast(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1, last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                left = mid + 1; // keep looking right
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        if(ans.empty()){
-            return {-1,-1};
-        }
-        else{
-            return {ans.front(),ans.back()};
-        }
-        
+        return last;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        return {findFirst(nums, target), findLast(nums, target)};
     }
 };
