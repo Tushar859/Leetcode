fecd0d16e586c1fc3058a28c41002474fefa2f1c
Tushar859 <mishrat859@gmail.com>
2026-01-29 00:26:11 +0530

    Time: 59 ms (5.43%) | Memory: 17.2 MB (22.37%) - LeetSync

--- 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     string sortVowels(string s) {
-        string vowels = "aeiouAEIOU";
-        unordered_set<char> vowelSet(vowels.begin(), vowels.end());
-        
-        // Step 1: extract vowels
-        vector<char> vowelList;
-        for (char c : s) {
-            if (vowelSet.count(c)) {
-                vowelList.push_back(c);
+        priority_queue<char,vector<char>,greater<char>>pq;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='o' || s[i]=='i'|| s[i]=='u'){
+                pq.push(s[i]);
             }
-        }
-        
-        // Step 2: sort vowels by ASCII
-        sort(vowelList.begin(), vowelList.end());
-        
-        // Step 3: rebuild string
-        int idx = 0;
-        for (char &c : s) {
-            if (vowelSet.count(c)) {
-                c = vowelList[idx++];
+
+            if(s[i]=='A'||s[i]=='E'||s[i]=='O' || s[i]=='I'|| s[i]=='U'){
+                pq.push(s[i]);
             }
+
         }
-        
-        return s;
+            string ans;
+       
+             for(int i=0;i<s.length();i++){
+            if(s[i]=='a'||s[i]=='e'||s[i]=='o' || s[i]=='i'|| s[i]=='u'){
+               char val = pq.top();  
+                  pq.pop();             
+
+               ans+=val;
+            }
+
+            else if(s[i]=='A'||s[i]=='E'||s[i]=='O' || s[i]=='I'|| s[i]=='U'){
+                  char val = pq.top();  
+                  pq.pop(); 
+
+
+               ans+=val;
+            }
+            else{
+                    ans+=s[i];
+            }
+         
+
+        }
+
+
+        return ans;
+
+
     }
 };
