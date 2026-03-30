b6d5f46b7779a11dd7f7e02828666743b8ad3a51
Tushar Mishra <mishrat859@gmail.com>
2026-03-30 13:52:25 +0530

    Time: 0 ms (100.00%) | Memory: 15 MB (14.43%) - LeetSync

---
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+    int sum =0;
+    int n= mat.size();
+    for(int i=0;i<mat.size();i++){
+        sum+=mat[i][i];
+        if(i!=n-i-1){
+        sum+=mat[i][n-i-1]; // isse hum 2nd diagonal elements ko dekh rhe duplicate remove kr rhe haain
+        }
+       
+    }
+    
+    return sum;
+    }
+};
