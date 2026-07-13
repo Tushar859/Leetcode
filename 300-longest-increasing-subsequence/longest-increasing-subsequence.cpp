2a93565fa3aa758b0830b948ec32e904be600e5d
Tushar Mishra <mishrat859@gmail.com>
2026-07-13 23:26:56 +0530

    Time: 648 ms (5.04%) | Memory: 355.3 MB (6.58%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+     int solvebyrec(vector<int>& nums, int curr , int prev , vector<vector<int>>& dp){
+    // base case
+    if(curr >= nums.size()){
+        return 0;
+    }
+    if(dp[curr][prev+1]!=-1){
+        return dp[curr][prev+1];
+    }
+    // incldue
+    int include =0;
+    if(prev==-1 || nums[curr]>nums[prev]){
+         include = 1 + solvebyrec(nums,curr+1,curr,dp);
+    }
+    
+
+    // exclude
+     int exclude = 0 + solvebyrec(nums,curr+1,prev,dp);
+
+    int ans = max(include ,exclude);
+        dp[curr][prev+1] = ans;
+    return ans;
+    
+     }
+// two var are changing so 2d dp will be used
+    int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<vector<int>> dp(n+200,vector<int>(n+200,-1));
+    int curr =0;
+    int prev =-1;
+    return solvebyrec(nums,curr,prev,dp);
+    }
+};
