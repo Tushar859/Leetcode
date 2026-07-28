c45845048729084834a9f1b031062b88a1ec8e33
Tushar Mishra <mishrat859@gmail.com>
2026-07-28 19:25:18 +0530

    Time: 7 ms (20.55%) | Memory: 10.8 MB (80.90%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        
-        unordered_map<char,int> freq;
-        
-     
-        for(int i = 0; i < s.length(); i++){
-            freq[s[i]]++;
-        }
+        string op="";
+       map<char,int>freq;
+       for(int i=0;i<s.length();i++){
+          freq[s[i]]++;
+       } 
 
+       priority_queue<pair<int,char>>ans;
+       for(auto x : freq){
+           ans.push({x.second , x.first});
+       }
+    
+       while(!ans.empty()){
+          int count = ans.top().first;
+          char ch = ans.top().second;
+            ans.pop();
+         while(count!=0){
+            op+=ch;
+            count--;
+         }
 
-        priority_queue<pair<int,char>> p;
-        
-        for(auto it : freq){
-            p.push({it.second, it.first});
-        }
-
-        string ans = "";
-
-        while(!p.empty()){
-            int count = p.top().first;
-            char ch = p.top().second;
-            p.pop();
-
-     
-            while(count--){
-                ans += ch;
-            }
-        }
-
-        return ans;
+       }
+        return op;
+       
     }
 };
