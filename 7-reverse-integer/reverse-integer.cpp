f912adbeaeab15d8db83a4e7df6785e5154a84e5
Tushar Mishra <mishrat859@gmail.com>
2026-07-29 13:36:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (5.03%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,17 +1,50 @@
 class Solution {
 public:
-    int reverse(int x) {
-        int rev = 0;
-        while (x != 0) {
-            int digit = x % 10;
-            x /= 10;
-
-            // Check for overflow before multiplying by 10
-            if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7)) return 0;
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8)) return 0;
-
-            rev = rev * 10 + digit;
+    void rev (string &num){
+        int start =0;
+        int end = num.size()-1;
+        while(start<end){
+            char temp = num[start];
+            num[start]=num[end];
+            num[end] = temp;
+            start++;
+            end--;
         }
-        return rev;
+    }
+    int reverse(int x) {
+       bool neg = false;
+        string num = to_string(x);
+         if (num[0] == '-') {
+            neg = true;
+            num.erase(0, 1);
+        }
+         rev(num);
+        
+        // lets remove leading zero
+        int i=0;
+        while(i<num.size()&&num[i]==0){
+            i++;}
+
+
+         if(i==num.size()){
+            // iska mtlb sare zeros h
+            return 0;
+         } 
+        
+        num = num.substr(i);
+     
+    long long ans = stoll(num);
+     if(neg){
+        ans = -ans;
+     }
+     // Overflow check
+        if (ans > INT_MAX || ans < INT_MIN)
+            return 0;
+
+
+
+
+    return ans;
+    
     }
 };
