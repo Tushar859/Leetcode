1c9e3b5434dfde7ab0da45256dc22c6acf396e03
Tushar Mishra <mishrat859@gmail.com>
2026-08-01 18:21:32 +0530

    Time: 59 ms (8.27%) | Memory: 19.3 MB (7.58%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -4,7 +4,7 @@ public:
         int n = s.size();
         int start = 0, end = 0;
         int len = 0;
-        unordered_map<char, int> freq;
+        unordered_map<int, int> freq;
         while (end < n) {
             freq[s[end]]++;
             while (freq[s[end]] > 1) {
