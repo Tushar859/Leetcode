6065d90f1f060a417194dc9880ef46c5edc2be45
Tushar Mishra <mishrat859@gmail.com>
2026-08-03 17:01:08 +0530

    Time: 10 ms (45.27%) | Memory: 11.3 MB (86.71%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -1,4 +1,4 @@
-/**
+                                                                    /**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -9,32 +9,34 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        if(head == nullptr) 
-        {return nullptr;}
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-       
-        while(fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if(slow == fast)
-                break;
-        }
-
-    
-        if(fast == nullptr || fast->next == nullptr)
-            return nullptr;
-
-
-        slow = head;
-        while(slow != fast) {
-            slow = slow->next;
-            fast = fast->next;
-        }
-
-        return slow; 
+        // if(head == nullptr){
+        //     return nullptr;
+        // }
+        // if(head->next == nullptr){
+        //     return  nullptr;
+        // }
+       ListNode* fast = head;
+       ListNode* slow = head;
+       while(fast!=nullptr && fast->next!=nullptr){
+         slow = slow->next;
+         fast = fast->next->next;
+         if(slow == fast){
+            break ;
+         }
+        
+       }
+       // check whether cycke was ir nit
+            if (fast == nullptr || fast->next == nullptr) {
+                return nullptr;
+                }
+       // yha aaye mtlb slow fast mil chuke
+       slow = head;
+       // slow fast ek step move krege
+       while(fast!=slow){
+        slow = slow->next;
+        fast = fast->next;
+       }
+       return slow;
+        
     }
 };
