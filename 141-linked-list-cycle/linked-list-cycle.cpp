2f6b4bcbd652b95edf266e13f74e6cd9f51b3be9
Tushar Mishra <mishrat859@gmail.com>
2026-08-03 11:54:11 +0530

    Time: 6 ms (92.03%) | Memory: 11.9 MB (23.72%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,30 +9,27 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        // null ll sambhal lia
-        if(head == NULL){
-            return false;
+      // single element;
+      if(head == nullptr){
+        return false;
+      }
+      if(head->next == nullptr){
+        return false;
+      }
+      
+      ListNode* slow = head;
+      ListNode* fast = head;
+      while(fast!=nullptr && fast->next!=nullptr){
+        fast = fast->next->next;
+        slow= slow->next;
+        if(slow == fast){
+            return true;
+            break;
         }
-        //single element sambhal lia
-        if(head->next == NULL){
-            return false;
-        }
-
-     
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast!= NULL){
-            // ek step fast 
-            fast = fast->next;
-            if(fast != NULL){
-                fast = fast->next;
-                if(fast== slow){
-                    return true;
-                }
-                slow = slow->next;
-            }
-        }
+      }
     return false;
+    
+
+
     }
 };
