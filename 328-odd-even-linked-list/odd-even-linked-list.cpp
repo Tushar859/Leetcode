73deb4abcd8b7f860819a7a03cc07034349fc34c
Tushar Mishra <mishrat859@gmail.com>
2026-08-03 19:32:03 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (5.17%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -8,34 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr)
-            return head;
-
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenHead = even;
-
-        ListNode* temp = even->next;   
-
-        while (temp != nullptr) {
-            odd->next = temp;          
-            odd = temp;
-
-            if (temp->next == nullptr){
-                break;
-            }
-
-            even->next = temp->next;  
-            even = temp->next;
-
-            temp = temp->next->next;   
-        }
-         even->next = nullptr;
-        odd->next = evenHead;
+        // empty and sigle ele handle kr dia
+      if(head==nullptr || head->next ==nullptr){
         return head;
+      }
+      vector<int>ans;
+       ListNode* temp = head;
+       while(temp!=nullptr){
+         ans.push_back(temp->val);
+         temp=temp->next;
+       }
+       ListNode* newNode = new ListNode(-1);
+         ListNode* head1 = newNode;
+          ListNode* tail = newNode;
+        for(int i=0;i<ans.size();i=i+2){
+            ListNode* temp = new ListNode(ans[i]);
+            tail->next = temp;
+            tail = tail->next;
+        }
+         for(int i=1;i<ans.size();i=i+2){
+            ListNode* temp = new ListNode(ans[i]);
+            tail->next = temp;
+            tail = tail->next;
+        }
+     return newNode->next;
+      
+        
     }
 };
