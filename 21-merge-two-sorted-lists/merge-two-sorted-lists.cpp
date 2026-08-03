ee1a60c1a96123a98d611f24bbf10f52320b5f69
Tushar Mishra <mishrat859@gmail.com>
2026-08-03 22:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 19.9 MB (8.99%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,56 +11,28 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1 ==nullptr && list2 == nullptr){
-            return nullptr;
+        vector<int> v;
+        ListNode* temp = list1;
+        while(temp != nullptr) {
+            v.push_back(temp->val);
+            temp = temp->next;
         }
-        // single element
-        if(list1 != nullptr && list2 ==nullptr){
-            return list1;
+        temp = list2;
+        while(temp != nullptr) {
+            v.push_back(temp->val);
+            temp = temp->next;
         }
-         if(list2!= nullptr && list1 ==nullptr){
-            return list2;
+        sort(v.begin(), v.end());
+
+        ListNode* dummy = new ListNode(-1);
+        ListNode* tail = dummy;
+
+        for(int i = 0; i < v.size(); i++) {
+            ListNode* newNode = new ListNode(v[i]);
+            tail->next = newNode;
+            tail = newNode;
         }
-         ListNode* curr = new ListNode();
 
-       ListNode* temp1 = list1;
-       ListNode* temp2 =  list2;
-       ListNode* cur = curr;
-
-       while(temp1!=NULL && temp2!=NULL ){
-        if(temp1->val <= temp2->val){
-            // daal do
-            //cur ->val = temp1->val;
-         // iss trh dalte hain new element   
-        cur->next = new ListNode(temp1->val);
-            cur = cur->next;
-            temp1 =temp1->next;
-
-        }
-        else{
-           //temp1->val <= temp2->val
-          // cur ->val = temp2->val;
-          
-        cur->next = new ListNode(temp2->val);
-            cur = cur->next;
-           temp2 = temp2->next;
-
-        }
-    
-       }
-   while(temp1!=NULL){
-         cur->next = new ListNode(temp1->val);
-          cur = cur->next;
-         temp1 = temp1->next;
-           }
- while(temp2!=NULL){
-         cur->next = new ListNode(temp2->val);
-          cur = cur->next;
-         temp2 = temp2->next;
-              }
-
- return curr->next;
-
-        
+        return dummy->next;
     }
 };
