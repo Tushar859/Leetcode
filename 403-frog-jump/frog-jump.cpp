df9e521f0d453131f95d5aa024a30caba83600a2
Tushar Mishra <mishrat859@gmail.com>
2026-09-12 20:19:24 +0530

    Time: 97 ms (56.82%) | Memory: 240.5 MB (26.10%) - LeetSync

--- 403-frog-jump/frog-jump.cpp
+++ 403-frog-jump/frog-jump.cpp
@@ -1,44 +1,49 @@
 class Solution {
 public:
-    bool solve(int index,int k,vector<int>& stones,unordered_map<int,int>& mp,vector<vector<int>>& dp){
+    vector<vector<int>> dp;
+
+    bool solve(int pos,int k,vector<int>& stones,unordered_map<int,int>& mp){
         // last stone reached
-        if(index==stones.size()-1){
+        if(pos==stones.size()-1){
             return true;
         }
 
-        if(dp[index][k]!=-1)
-            return dp[index][k];
+        if(dp[pos][k]!=-1){
+            return dp[pos][k];
+        }
 
         // k-1 , k, k+1 valid moves are allowerd
-        int next=stones[index]+(k-1);
+        int next=stones[pos]+(k-1);
         bool op1=false;
         if(k-1>0&&mp.count(next))
-            op1=solve(mp[next],k-1,stones,mp,dp);
+            op1=solve(mp[next],k-1,stones,mp);
 
         bool op2=false;
-        next=stones[index]+(k+1);
+        next=stones[pos]+(k+1);
         if(mp.count(next))
-            op2=solve(mp[next],k+1,stones,mp,dp);
+            op2=solve(mp[next],k+1,stones,mp);
 
         bool op3=false;
-        next=stones[index]+k;
+        next=stones[pos]+k;
         if(mp.count(next))
-            op3=solve(mp[next],k,stones,mp,dp);
+            op3=solve(mp[next],k,stones,mp);
 
-        return dp[index][k]=op1||op2||op3;
+        return dp[pos][k]=op1||op2||op3;
     }
 
     bool canCross(vector<int>& stones){
         unordered_map<int,int>mp;
+
         for(int i=0;i<stones.size();i++){
             mp[stones[i]]=i;
         }
 
-        if(stones[1]!=1)
+        if(stones[1]!=1){
             return false;
+        }
 
-        vector<vector<int>>dp(stones.size(),vector<int>(stones.size()+1,-1));
+        dp.assign(stones.size()+1,vector<int>(stones.size()+1,-1));
 
-        return solve(1,1,stones,mp,dp);
+        return solve(1,1,stones,mp);
     }
 };
