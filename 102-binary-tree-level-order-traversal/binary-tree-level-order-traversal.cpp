cf2cbf0d0e6a0d2d019e483e5c6d55c22c508df7
Tushar Mishra <mishrat859@gmail.com>
2026-09-18 21:41:29 +0530

    Time: 1 ms (39.08%) | Memory: 17 MB (90.46%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-
         vector<vector<int>> ans;
-
-        if (root == nullptr)
+        queue<pair<TreeNode*, int>> q;
+        if(root == NULL){
             return ans;
-
-        queue<TreeNode*> q;
-
-        q.push(root);
-        q.push(nullptr);
-
-        vector<int> level;
-
-        while (!q.empty()) {
-
-            TreeNode* front = q.front();
-            q.pop();
-
-            // Level complete
-            if (front == nullptr) {
-
-                ans.push_back(level);
-                level.clear();
-
-                // Agar queue mein abhi nodes hain,
-                // toh next level ke end ke liye nullptr
-                if (!q.empty())
-                    q.push(nullptr);
-
+        }
+        // root - level
+        q.push({root, 0});
+        while(!q.empty()){
+            vector<int> l;
+            int n = q.size();
+            for(int i = 0; i < n; i++){
+                auto it = q.front();
+                TreeNode* val = it.first;
+                int level = it.second;
+                q.pop();
+                l.push_back(val->val);
+                if(val->left){
+                    q.push({val->left, level + 1});
+                }
+                if(val->right){
+                    q.push({val->right, level + 1});
+                }
             }
-            else {
-
-                level.push_back(front->val);
-
-                if (front->left != nullptr)
-                    q.push(front->left);
-
-                if (front->right != nullptr)
-                    q.push(front->right);
-            }
+            ans.push_back(l);
         }
 
         return ans;
